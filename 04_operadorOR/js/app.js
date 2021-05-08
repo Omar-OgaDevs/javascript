8ccfda41c4ecc21b3362363f7a777629fead2499
omar.ogadevs <omar.ogadevs@outlook.com>
2021-05-08 15:03:10 -0500

    operardor OR

---
+++ 04_operadorOR/js/app.js
@@ -0,0 +1,8 @@
+let vacaciones = false, diaDescanso = true;
+
+if(vacaciones || diaDescanso){
+  console.log("Tu papá te puede llevar al cine");
+}
+else{
+  console.log("Tu papá tiene que ir al trabajo");
+}
