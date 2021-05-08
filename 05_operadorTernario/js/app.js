6cf667d4d73adaa6e0d210cec6a2ea0c84a59eaf
omar.ogadevs <omar.ogadevs@outlook.com>
2021-05-08 15:12:05 -0500

    operador ternario

---
+++ 05_operadorTernario/js/app.js
@@ -0,0 +1,7 @@
+let resultado = (5>7) ? "cierto" : "mentira";
+console.log(resultado);
+
+/*numero para con operador ternario ? */
+let numero = 9;
+resultado = (numero % 2 == 0) ? "número par" : "número impar";
+console.log(resultado);
