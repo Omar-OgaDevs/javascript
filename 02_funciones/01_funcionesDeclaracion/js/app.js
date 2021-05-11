f622a70cf4652ae9c0a9d691e70067244ef941cc
omar.ogadevs <omar.ogadevs@outlook.com>
2021-05-11 12:02:35 -0500

    declaracion de funciones en javascript

---
+++ 02_funciones/01_funcionesDeclaracion/js/app.js
@@ -0,0 +1,11 @@
+//Hoisting 
+miFuncion(7,2);
+
+//Declararcion de la función
+//puede tener o no argumentos dentro de ()
+function miFuncion(a,b){
+  console.log("suma: " + (a+b));
+}
+//Llamando a la función
+//Hoisting
+miFuncion(9,7);
