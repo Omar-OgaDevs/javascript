2fd0d87ac07a040b17cb94e4188eeb266db3c5b1
omar.ogadevs <omar.ogadevs@outlook.com>
2021-05-11 13:17:08 -0500

    comparacion

--- 02_funciones/05_funcionesFlecha/js/app.js
+++ 02_funciones/05_funcionesFlecha/js/app.js
@@ -1,6 +1,11 @@
-//Declaracion de función tipo flecha
+//funcion de tipos expresión
+/*
+let sumar = function (a,b){return a + b};
+let resultado = sumar(7,2);
+console.log(resultado);
+*/
+//función de tipo expresión a función de tipo flecha
 //se recomienda usar CONST en lugar de LET
-
 const funcionFlecha = (a,b) => a+b;
 let resultado = funcionFlecha(7,2);
 console.log(resultado);
