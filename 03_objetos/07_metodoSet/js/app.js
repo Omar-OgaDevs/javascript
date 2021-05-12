6e745ce5cce937b464302890fc15ea6cd3583e7f
omar.ogadevs <omar.ogadevs@outlook.com>
2021-05-12 10:17:57 -0500

    método set en objetos de javascript

---
+++ 03_objetos/07_metodoSet/js/app.js
@@ -0,0 +1,23 @@
+let alumno = {
+  nombre: 'Evelyn',
+  apellido: 'Garcia',
+  grupo: '2A',
+  edad: 7,
+  tutor: 'Marcela',
+  idioma: 'es',
+  get lang(){
+    return this.idioma.toUpperCase();
+  },
+  //Método set
+  set lang(lang){
+    this.idioma = lang.toUpperCase();
+  },
+  get nombreCompleto(){
+    return this.nombre + ' ' + this.apellido;
+  }
+}
+console.log(alumno.lang);
+
+alumno.lang = 'en';
+
+console.log(alumno.lang);
