289c0ee2ed4107e63ea8846b6d054cb6f847d4ff
omar.ogadevs <omar.ogadevs@outlook.com>
2021-05-11 20:44:58 -0500

    objetos en javascript

---
+++ 03_objetos/01_objetosJavaScript/js/app.js
@@ -0,0 +1,11 @@
+let persona ={
+  nombre: 'Evelyn',
+  apellido: 'García',
+  email: 'ivin@babidibu.com',
+  edad: 7
+}
+console.log(persona.nombre);
+console.log(persona.edad);
+console.log(persona.apellido);
+
+console.log(persona);
