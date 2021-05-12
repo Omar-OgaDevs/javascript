e31a594b743151bbef7c59068607bcf62e53a35b
omar.ogadevs <omar.ogadevs@outlook.com>
2021-05-12 15:43:06 -0500

    distintas formas de crear objetos en javascript

---
+++ 03_objetos/10_formasCrearObjetos/js/app.js
@@ -0,0 +1,19 @@
+//Sintaxis formal
+let miObjeto1 = new Object();
+//Sintaxis recomendada
+let miObjeto2 = {};
+
+let miCadena1 = new String('Hola');
+let miCadena2 = 'Hola';
+
+let miNumero1 = new Number(7);
+let miNumero2 = 7;
+
+let miBoolean1 = new Boolean(false);
+let miBoolean2 = false;
+
+let miArreglo1 = new Array();
+let miArreglo2 = [];
+
+let miFuncion1 = new Function();
+let miFuncion2 = function(){};
