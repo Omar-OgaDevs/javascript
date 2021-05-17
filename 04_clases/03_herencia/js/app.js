936a8d9073ba43e213ea685f6a3a5a85a73392ba
omar.ogadevs <omar.ogadevs@outlook.com>
2021-05-17 15:59:58 -0500

    herencia

---
+++ 04_clases/03_herencia/js/app.js
@@ -0,0 +1,36 @@
+class Persona {
+  constructor(nombre, apellido) {
+    this._nombre = nombre;
+    this._apellido = apellido;
+  }
+  get nombre() {
+    return this._nombre;
+  }
+  set nombre(nombre) {
+    this._nombre = nombre;
+  }
+  get apellido(){
+    return this._apellido;
+  }
+  set apellido(apellido){
+    this._apellido = apellido;
+  }
+}
+class Empleado extends Persona{
+  constructor(nombre, apellido,departamento){
+    super(nombre,apellido); //llamar al constructor de la clase padre
+    this._departamento = departamento;
+  }
+  get departamento(){
+    return this._departamento;
+  }
+  set departamento(departamento){
+    this._departamento = departamento;
+  }
+}
+
+let persona1 = new Persona('Omar','García');
+console.log(persona1); //get nombre
+
+let empleado1 = new Empleado('Evelyn','García','Sistemas');
+console.log(empleado1);
