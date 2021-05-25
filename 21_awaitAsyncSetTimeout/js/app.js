1648be900713913d58de60be1adce0f591fb7218
omar.ogadevs <omar.ogadevs@outlook.com>
2021-05-25 11:46:51 -0500

    await async setTimeout promise

---
+++ 21_awaitAsyncSetTimeout/js/app.js
@@ -0,0 +1,34 @@
+let miPromesa = new Promise((resolver, rechazar) => {
+  let expresion = true;
+  if (expresion) resolver("Resolvio Correctamente");
+  else rechazar("Se produjo un error");
+});
+
+let promesa = new Promise((resolver) => {
+  setTimeout(() => resolver("hola con promesa y timeout"), 3000);
+});
+
+//async indica que una función regresa una promesa
+
+async function miFuncionConPromesa() {
+  return "saludos con async y promesa";
+}
+
+//async con await
+
+async function funcionConPromesaYAwait() {
+  let miPromesa = new Promise (resolver => {
+    resolver("Promesa con await");
+  });
+  console.log(await miPromesa);
+}
+
+//promesas, await, async y setTimeout
+
+async function funcionConPromesaAwaitTimeout(){
+  let miPromesa = new Promise (resolver=>{
+    setTimeout(()=>resolver('promesa con await y timeout'),3000);
+  });
+  console.log(await miPromesa);
+}
+funcionConPromesaAwaitTimeout();
